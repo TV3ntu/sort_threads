85fb394ce5fffb86db4abef7960cbe80e6e53348
ilrosaunsam <ilrosa@estudiantes.unsam.edu.ar>
2023-11-13 15:44:39 -0300

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,116 +1,156 @@
 import threading
-import random
 import time
+import random
 import matplotlib.pyplot as plt
-import psutil
 
-
-def bubblesort(arr, num_threads=1):
+def bubble_sort(arr):
     n = len(arr)
-    chunk_size = n // num_threads
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+def sort_with_threads(arr, num_threads):
+    chunk_size = len(arr) // num_threads
     threads = []
 
-    for i in range(num_threads):
-        start_index = i * chunk_size
-        end_index = (i + 1) * chunk_size if i < num_threads - 1 else n
-        thread = threading.Thread(target=bubblesort_part, args=(arr, start_index, end_index))
+    for _ in range(num_threads):
+        thread = threading.Thread(target=bubble_sort, args=(arr,))
         threads.append(thread)
+
+    start_time = time.time()
+
+    for thread in threads:
         thread.start()
 
     for thread in threads:
         thread.join()
 
-    # Merge de las partes ordenadas
-    return merge(arr, num_threads)
+    elapsed_time = time.time() - start_time
 
+    return elapsed_time
 
-def bubblesort_part(arr, start, end):
-    for i in range(start, end):
-        for j in range(start, end - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+def run_test(num_threads, num_elements):
+    arr = [random.randint(1, 100000000) for _ in range(num_elements)]
 
+    print(f"\nCon {num_threads} hilos y {num_elements} elementos:")
+    # print("Lista desordenada:", arr) DESCOMENTAR PARA VER LISTA ORDENADA
 
-def merge(arr, num_threads):
+    elapsed_time = sort_with_threads(arr, num_threads)
+
+    # print("Lista ordenada:", arr) DESCOMENTAR PARA VER LISTA ORDENADA
+    print(f"Tardó {elapsed_time:.4f} segundos\n")
+
+    return num_threads, elapsed_time
+
+def plot_graph(title, x_label, y_label, data):
+    x, y = zip(*data)
+    plt.plot(x, y, marker='o')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
+
+def main():
+    element_count_5 = 100
+    element_count_10 = 10000
+    thread_counts = [1, 20, 40]
+    results_5 = []
+    results_10 = []
+
+    # Pruebas con 5 elementos
+    for num_threads in thread_counts:
+        elapsed_time = run_test(num_threads, element_count_5)
+        results_5.append(elapsed_time)
+
+    # Gráfico para 5 elementos
+    plot_graph("Bubble Sort con 100 elementos", "Cantidad de Hilos", "Tiempo (segundos)", results_5)
+
+    # Pruebas con 10 elementos
+    for num_threads in thread_counts:
+        elapsed_time = run_test(num_threads, element_count_10)
+        results_10.append(elapsed_time)
+
+    # Gráfico para 10 elementos
+    plot_graph("Bubble Sort con 10000 elementos", "Cantidad de Hilos", "Tiempo (segundos)", results_10)
+
+if __name__ == "__main__":
+    main()
+import threading
+import time
+import random
+import matplotlib.pyplot as plt
+
+def bubble_sort(arr):
     n = len(arr)
-    merged_arr = [0] * n
-
-    # Dividir el arreglo en partes ordenadas por los threads
-    chunk_size = n // num_threads
-    chunks = [arr[i * chunk_size:(i + 1) * chunk_size] for i in range(num_threads)]
-
-    # Inicializar índices para cada chunk
-    indices = [0] * num_threads
-
-    # Combinar las partes ordenadas
     for i in range(n):
-        min_val = float('inf')
-        min_index = -1
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-        for j in range(num_threads):
-            if indices[j] < len(chunks[j]) and chunks[j][indices[j]] < min_val:
-                min_val = chunks[j][indices[j]]
-                min_index = j
+def sort_with_threads(arr, num_threads):
+    chunk_size = len(arr) // num_threads
+    threads = []
 
-        indices[min_index] += 1
-        merged_arr[i] = min_val
-
-    return merged_arr
-
-
-def run_comparison_bubble(arr_size, num_threads):
-    data = [random.randint(1, 1000) for _ in range(arr_size)]
-
-    # Iniciar el monitoreo de recursos
-    process = psutil.Process()
-    initial_memory = process.memory_info().rss
-    initial_cpu = process.cpu_percent()
+    for _ in range(num_threads):
+        thread = threading.Thread(target=bubble_sort, args=(arr,))
+        threads.append(thread)
 
     start_time = time.time()
-    bubblesort(data.copy(), num_threads)
-    end_time = time.time()
 
-    # Finalizar el monitoreo de recursos
-    final_memory = process.memory_info().rss
-    final_cpu = process.cpu_percent()
+    for thread in threads:
+        thread.start()
 
-    # Retornar el tiempo de ejecución y el uso de recursos
-    return end_time - start_time, final_memory - initial_memory, final_cpu - initial_cpu
+    for thread in threads:
+        thread.join()
 
+    elapsed_time = time.time() - start_time
 
-if __name__ == '__main__':
-    sizes = [10, 100, 1000, 10000]
-    thread_options = [1, 2, 4, 8]
+    return elapsed_time
 
-    # Crear el gráfico con tamaño personalizado
-    plt.figure(figsize=(12, 8))
+def run_test(num_threads, num_elements):
+    arr = [random.randint(1, 100000000) for _ in range(num_elements)]
 
-    for threads in thread_options:
-        times_with_threads_bubble = []
-        memory_usage_with_threads_bubble = []
-        cpu_usage_with_threads_bubble = []
+    print(f"\nCon {num_threads} hilos y {num_elements} elementos:")
+    # print("Lista desordenada:", arr) DESCOMENTAR PARA VER LISTA ORDENADA
 
-        for size in sizes:
-            time_with_threads, memory_with_threads, cpu_with_threads = run_comparison_bubble(size, threads)
-            times_with_threads_bubble.append(time_with_threads)
-            memory_usage_with_threads_bubble.append(memory_with_threads)
-            cpu_usage_with_threads_bubble.append(cpu_with_threads)
+    elapsed_time = sort_with_threads(arr, num_threads)
 
-        # Graficar el tiempo de ejecución para Bubble Sort
-        plt.subplot(2, 1, 1)
-        plt.plot(sizes, times_with_threads_bubble, label=f'Con {threads} Threads')
-        plt.xlabel('Tamaño de la Lista')
-        plt.ylabel('Tiempo de Ejecución (segundos)')
-        plt.title('Comparación de Bubble Sort con Diferentes Cantidades de Threads')
-        plt.legend()
+    # print("Lista ordenada:", arr) DESCOMENTAR PARA VER LISTA ORDENADA
+    print(f"Tardó {elapsed_time:.4f} segundos\n")
 
-        # Graficar el uso de memoria para Bubble Sort
-        plt.subplot(2, 1, 2)
-        plt.plot(sizes, memory_usage_with_threads_bubble, label=f'Con {threads} Threads', marker='o')
-        plt.xlabel('Tamaño de la Lista')
-        plt.ylabel('Uso de Memoria (bytes)')
-        plt.title('Uso de Memoria con Diferentes Cantidades de Threads')
-        plt.legend()
+    return num_threads, elapsed_time
 
-    plt.tight_layout()
+def plot_graph(title, x_label, y_label, data):
+    x, y = zip(*data)
+    plt.plot(x, y, marker='o')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.show()
+
+def main():
+    element_count_5 = 100
+    element_count_10 = 10000
+    thread_counts = [1, 20, 40]
+    results_5 = []
+    results_10 = []
+
+    # Pruebas con 5 elementos
+    for num_threads in thread_counts:
+        elapsed_time = run_test(num_threads, element_count_5)
+        results_5.append(elapsed_time)
+
+    # Gráfico para 5 elementos
+    plot_graph("Bubble Sort con 100 elementos", "Cantidad de Hilos", "Tiempo (segundos)", results_5)
+
+    # Pruebas con 10 elementos
+    for num_threads in thread_counts:
+        elapsed_time = run_test(num_threads, element_count_10)
+        results_10.append(elapsed_time)
+
+    # Gráfico para 10 elementos
+    plot_graph("Bubble Sort con 10000 elementos", "Cantidad de Hilos", "Tiempo (segundos)", results_10)
+
+if __name__ == "__main__":
+    main()
